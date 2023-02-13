19faf496939855d5e857f69bdf21a3b74d62c3c0
Yean1277 <78299273+Yean1277@users.noreply.github.com>
2023-02-13 10:50:23 +0800

    Create type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,16 @@
+# Type Conversion
+birth_year = input("Enter your birth year: ") # return string
+age = 2022 - birth_year # Error(2022 is int, birth_year is str)
+print(age)
+
+# Solve this problem
+birth_year = input("Enter your birth year: ")
+int(birth_year) # converse str to int
+age = 2022 - birth_year
+print(age)
+
+
+other function for coverting
+float()
+bool()
+str()
