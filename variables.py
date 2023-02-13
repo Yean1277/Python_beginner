4ada8bbd203be9c073386c7d5bc7893e011f7c88
Yean1277 <78299273+Yean1277@users.noreply.github.com>
2023-02-13 10:38:32 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Integer
+age = 20
+
+# String
+# single quote also accepted
+name = "Ash"
+
+# Boolean
+# Important: make sure the first alphabet is CAPITAL LETTER
+status = True
+
+print(age)
+print(name)
+print(status)
